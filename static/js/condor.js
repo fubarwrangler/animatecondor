a15adde9239f70aed83e6116b79b83f8925019cc
William Strecker-Kellogg <willsk@bnl.gov>
2017-09-07 16:25:02 -0400

    Update skeleton JS and image

--- static/js/condor.js
+++ static/js/condor.js
@@ -1,7 +1,10 @@
 var svgns = "http://www.w3.org/2000/svg";
 
+$.fn.exists = function () {
+    return this.length !== 0;
+};
 
-function createCircle (event) {
+function createCircle () {
   var svg = this.getSVGDocument().documentElement;
   console.log(svg);
   var circle = document.createElementNS(svgns, 'circle');
@@ -12,4 +15,23 @@ function createCircle (event) {
   svg.appendChild(circle);
 }
 
-$('#map').hover(createCircle);
+function animateCircle () {
+  var svg = this.getSVGDocument().documentElement;
+  if ($(svg).find('circle').exists()) {
+    var $circ = $(svg).find('circle.job');
+    console.log("Found Circle");
+    console.log($circ);
+  } else {
+    console.log("Create Circle");
+    var circle = document.createElementNS(svgns, 'circle');
+    circle.setAttributeNS(null, 'cx', 150);
+    circle.setAttributeNS(null, 'cy', 90);
+    circle.setAttributeNS(null, 'r', 10);
+    circle.setAttributeNS(null, 'style', 'fill: none; stroke: blue; stroke-width: 3px;' );
+    circle.setAttributeNS(null, 'class', 'job' );
+    svg.appendChild(circle);
+  }
+}
+
+//$('#map').on('load', createCircle);
+$('#map').hover(animateCircle);
